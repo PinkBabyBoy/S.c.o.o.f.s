b7ba7f5b7ad47ba213a9b351d8e4f0b6de63750f
pave.barinov <barinovp363@gmail.com>
2025-03-06 21:53:29 +0300

    temp commit (in refactoring)

--- features/file_browser/src/main/java/ru/barinov/file_browser/utils/FileSingleShareBus.kt
+++ features/file_browser/src/main/java/ru/barinov/file_browser/utils/FileSingleShareBus.kt
@@ -1,8 +1,21 @@
 package ru.barinov.file_browser.utils
 
-interface FileSingleShareBus<T> {
+import androidx.lifecycle.ViewModel
+import ru.barinov.core.InteractableFile
 
-    suspend fun get(): T?
+abstract class FileSingleShareBus<T> {
 
-    suspend fun share(data: T)
+    protected val holder = mutableMapOf<Key, T>()
+
+    abstract suspend fun get(key: Key): T?
+
+    abstract suspend fun share(key: Key, data: T)
+
+    enum class Key{
+        IMAGE_SHARE, ENCRYPTION
+    }
+
 }
+
+
+
--- features/file_browser/src/main/java/ru/barinov/file_browser/utils/FileSingleShareBusImpl.kt
+++ features/file_browser/src/main/java/ru/barinov/file_browser/utils/FileSingleShareBusImpl.kt
@@ -1,21 +1,21 @@
 package ru.barinov.file_browser.utils
 
+import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
 import ru.barinov.core.InteractableFile
+import ru.barinov.file_browser.viewModels.FileObserverViewModel
 
-internal class FileSingleShareBusImpl : FileSingleShareBus<InteractableFile> {
+internal class FileSingleShareBusImpl : FileSingleShareBus<InteractableFile>() {
 
     private val mutex = Mutex()
 
-    private var file: InteractableFile? = null
 
-    override suspend fun get() = mutex.withLock {
-        val temp = file
-        file = null
-        return@withLock temp
+    override suspend fun get(key: Key) = mutex.withLock {
+        holder.remove(key)
     }
 
 
-    override suspend fun share(data: InteractableFile) = mutex.withLock { this.file = data }
+    override suspend fun share(key: Key, data: InteractableFile) =
+        mutex.withLock { holder[key] = data }
 }
